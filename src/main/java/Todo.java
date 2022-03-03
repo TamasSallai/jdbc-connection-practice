5d9b036bfe30f81f20ed99b8e235a694c4374fa3
tamas <tamas.sallai.hu@gmail.com>
2022-03-03 14:49:45 +0100

    Created the Todo Pojo, and 3 methods in the TodoDao class for selecting and inserting to the database

---
+++ src/main/java/Todo.java
@@ -0,0 +1,39 @@
+public class Todo {
+    private final int id;
+    private final String name;
+    private final String description;
+    private final boolean done;
+
+    public Todo(int id, String name, String description, boolean done) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.done = done;
+    }
+
+    public Todo(String name, String description, boolean done) {
+        this(-1, name, description, done);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", done=" + done +
+                '}';
+    }
+}
--- src/main/java/TodoDao.java
+++ src/main/java/TodoDao.java
@@ -1,5 +1,9 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class TodoDao {
     private final String databasePath;
@@ -8,11 +12,65 @@ public class TodoDao {
         this.databasePath = databasePath;
     }
 
-    private Connection createConnection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(this.databasePath);
+    public void addTodo(Todo todo) {
+        try (Connection conn = createConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("insert into Todo (name, description, done) values (?,?,?)");
+            stmt.setString(1, todo.getName());
+            stmt.setString(2, todo.getDescription());
+            stmt.setBoolean(3, todo.isDone());
+            stmt.executeUpdate();
+            System.out.println("A record added to the table.");
+        } catch (SQLException e) {
+            System.out.println("Could not prepare statement to add Todo.");
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public Todo getTodo(int id) {
+        Todo todo = null;
+        try (Connection conn = createConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("select * from Todo where id = ?");
+            stmt.setInt(1, id);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                todo = new Todo(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getBoolean("done"));
+            }
         } catch (Exception e) {
+            System.out.println("Could not prepare statement to query a single Todo record!");
+            System.out.println("Error: " + e);
+        }
+        return todo;
+    }
+
+    public ArrayList<Todo> getAllTodos() {
+        ArrayList<Todo> todos = null;
+        try (Connection conn = createConnection()) {
+            todos = new ArrayList<>();
+            ResultSet resultSet = conn.createStatement().executeQuery("select * from Todo");
+            while (resultSet.next()) {
+                todos.add(new Todo(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getBoolean("done")));
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not prepare statement to query the records from the Todo table!");
+            System.out.println("Error: " + e);
+        }
+        return todos;
+    }
+
+    private Connection createConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(this.databasePath);
+        try {
+            conn.prepareStatement("create table if not exists Todo (id int auto_increment primary key, name varchar(255) not null, description varchar(255), done boolean not null)").execute();
+            System.out.println("A connection created.");
+        } catch (SQLException e) {
             System.out.println("Could not establish server connection.");
             System.out.println("Error: " + e);
         }
